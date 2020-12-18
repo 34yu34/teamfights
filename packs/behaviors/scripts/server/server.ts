cc9241f4ed1bd7203ca7ed7d83f25e7030262f70
Billy <billy.bouchard03@gmail.com>
2020-12-18 17:57:38 -0500

    added player adding

--- packs/behaviors/scripts/server/server.ts
+++ packs/behaviors/scripts/server/server.ts
@@ -1,60 +1,65 @@
 /// <reference types="minecraft-scripting-types-server" />
 
-class Player implements IEntity
-{
-	readonly __identifier__: string;
-	readonly __type__: "entity" | "item_entity";
-	readonly id: number;
-}
-
-class Team
-{
-    players : Player[]
-    id : number
-
-    constructor()
-    {
-		this.players = [];
-		this.id = 0;
-    }
-}
-
-class Game
-{
-    teams: Team[]
-    players: Player[]
-
-    constructor(players: Player[], noOfTeam: number = 2)
-    {
-        this.players = players
-        this.makeTeams(noOfTeam)
-    }
-
-    makeTeams(noOfTeam: number = 2)
-    {
-        this.players.sort(() => 0.5 - Math.random())
-
-		this.teams = new Array(noOfTeam).map(() => new Team())
-		
-        for (let i = 0; i < this.players.length; ++i)
-        {
-            this.teams[i % noOfTeam].players.push(this.players[i])
-		}
-		
-		for(let i = 0; i < this.teams.length; ++i)
-		{
-			this.teams[i].id = i
-		}
-    }
-}
-
-
 namespace Server {
 	const system = server.registerSystem(0, 0)
+
+	class Player implements IEntity {
+		readonly __identifier__: string;
+		readonly __type__: "entity" | "item_entity";
+		readonly id: number;
+		readonly name: string;
+
+		constructor(player: IEntity) {
+			this.__identifier__ = player.__identifier__;
+			this.__type__ = player.__type__;
+			this.id = player.id;
+			let component: IComponent<INameableComponent> = system.getComponent(player, MinecraftComponent.Nameable);
+			this.name = component.data.name;
+		}
+	}
+
+	class Team {
+		players: Player[]
+		id: number
+
+		constructor() {
+			this.players = [];
+			this.id = 0;
+		}
+	}
+
+	class Game {
+		teams: Team[]
+		players: Player[]
+
+		constructor(players: Player[], noOfTeam: number = 2) {
+			this.players = players
+			this.makeTeams(noOfTeam)
+		}
+
+		makeTeams(noOfTeam: number = 2) {
+			this.players.sort(() => 0.5 - Math.random())
+
+			this.teams = new Array(noOfTeam).map(() => new Team())
+
+			for (let i = 0; i < this.players.length; ++i) {
+				this.teams[i % noOfTeam].players.push(this.players[i])
+			}
+
+			for (let i = 0; i < this.teams.length; ++i) {
+				this.teams[i].id = i
+			}
+		}
+	}
+
 	const players: Player[] = []
 
 	// Setup which events to listen for
 	system.initialize = function () {
+
+		system.listenForEvent(ReceiveFromMinecraftServer.EntityDeath, onEntityDeath)
+		system.listenForEvent("teamfights:player_connected", addPlayer)
+
 		const scriptLoggerConfig = system.createEventData(SendToMinecraftServer.ScriptLoggerConfig)
 		scriptLoggerConfig.data.log_errors = true
 		scriptLoggerConfig.data.log_information = true
@@ -75,7 +80,7 @@ namespace Server {
 
 	const onEntityDeath = (event: IEventData<IEntityDeathEventData>) => {
 		if (event.data.entity.__identifier__ == "minecraft:player") {
-			makeObserver(event.data.entity)
+			makeObserver(new Player(event.data.entity))
 		}
 	}
 
@@ -87,8 +92,9 @@ namespace Server {
 	}
 
 	const addPlayer = (event: IEventData<IClientEnteredWorldEventData>) => {
-		players.push(event.data.player);
-		sendMessage(`Player ${event.data.player.id} has connected`)
+		const player = new Player(event.data.player);
+		players.push(player);
+		sendMessage(`Player ${player.name} has connected`)
 	}
 
 	const warnPlayersWorldBorderReducing = (newRadius: number, secondsLeft: number) => {
